645839e06d4d0614b599b5a2ee7a26e021ce88b4
eunmouse <ejha1208@gmail.com>
2024-09-02 12:43:11 +0900

    회원가입

--- src/main/java/org/example/springv3/user/UserService.java
+++ src/main/java/org/example/springv3/user/UserService.java
@@ -1,7 +1,9 @@
 package org.example.springv3.user;
 
 import lombok.RequiredArgsConstructor;
+import org.example.springv3.core.error.ex.Exception400;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -9,4 +11,16 @@ public class UserService {
     private final UserRepository userRepository;
     private final UserQueryRepository userQueryRepository;
 
+    @Transactional
+    public void 회원가입(User user) {
+
+        User userPs= userRepository.findByUsername(user.getUsername());
+
+        if(userPs != null) {
+            new Exception400("이미 존재하는 유저입니다.");
+        }
+
+        userRepository.save(user);
+
+    }
 }
