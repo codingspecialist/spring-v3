d311393fc442a129335ebdd36cbe3940daf7a573
chunsicks <ckddbsdbs123@naver.com>
2024-09-02 14:52:27 +0900

    로그인 기능완료 (#5)

--- src/main/java/org/example/springv3/user/UserRepository.java
+++ src/main/java/org/example/springv3/user/UserRepository.java
@@ -8,4 +8,9 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     @Query("select u from User u where u.username=:username")
     User findByUsername(@Param("username") String username);
+
+
+    @Query("select u from User u where u.username=:username and u.password=:password")
+    User findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
 }
+
--- src/main/java/org/example/springv3/user/UserRequest.java
+++ src/main/java/org/example/springv3/user/UserRequest.java
@@ -6,15 +6,12 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 public class UserRequest {
-
     @Data
     public static class JoinDTO {
         @NotEmpty
         private String username;
-
         @NotEmpty
         private String password;
-
         @Pattern(regexp = "^[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$", message = "이메일 형식으로 작성해주세요")
         @NotEmpty
         private String email;
@@ -22,5 +19,14 @@ public class UserRequest {
         public User toEntity() {
             return User.builder().username(username).password(password).email(email).build();
         }
+    }
+
+    @Data
+    public static class LoginDTO {
+        @NotEmpty
+        @Size(min = 2, max = 4)
+        private String username;
+        @NotEmpty
+        private String password;
     }
 }
