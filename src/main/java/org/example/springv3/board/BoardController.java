3d9c358572a8a3fcca7d8ae8e69e3e460917dcc4
wodud6967 <wodud6967@gmail.com>
2024-09-02 12:39:06 +0900

    게시물상세보기

--- src/main/java/org/example/springv3/board/BoardController.java
+++ src/main/java/org/example/springv3/board/BoardController.java
@@ -1,13 +1,32 @@
 package org.example.springv3.board;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.example.springv3.user.User;
 import org.example.springv3.user.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
 public class BoardController {
     private final HttpSession session;
     private final BoardService boardService;
+
+
+    @GetMapping("/board/{id}")
+    public String detail(@PathVariable("id") Integer id, HttpServletRequest request) {
+        User sessionUser = User.builder()
+                .id(1).username("ssar").password("1234").email("ssar@nate.com").build();
+        session.setAttribute("sessionUser", sessionUser);
+
+        BoardResponse.DetailDTO model = boardService.게시물상세보기(sessionUser, id);
+        request.setAttribute("model", model);
+
+        return "board/detail";
+
+
+    }
 }
--- src/main/java/org/example/springv3/board/BoardResponse.java
+++ src/main/java/org/example/springv3/board/BoardResponse.java
@@ -1,4 +1,35 @@
 package org.example.springv3.board;
 
+import lombok.Data;
+import org.example.springv3.user.User;
+
 public class BoardResponse {
+    @Data
+    public static class DetailDTO {
+
+        private Integer boardId;
+        private String title;
+        private String content;
+        private Boolean isOwner;
+        private Integer userId;
+        private String username;
+
+        public DetailDTO(Board board, User sessionUser) {
+            this.boardId = board.getId();
+            this.title = board.getTitle();
+            this.content = board.getContent();
+            this.isOwner = false;
+
+            if (sessionUser != null) {
+                if (board.getUser().getId() == sessionUser.getId()) {
+                    isOwner = true; // 권한체크
+
+                }
+            }
+
+
+            this.userId = board.getUser().getId();
+            this.username = board.getUser().getUsername();
+        }
+    }
 }
