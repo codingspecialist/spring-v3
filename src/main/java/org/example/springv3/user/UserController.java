e1bf63337fc64aeb8888c1c824e2f15b6f513133
eunmouse <ejha1208@gmail.com>
2024-09-02 14:47:33 +0900

    회원가입

--- src/main/java/org/example/springv3/user/UserController.java
+++ src/main/java/org/example/springv3/user/UserController.java
@@ -3,9 +3,14 @@ package org.example.springv3.user;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @RequiredArgsConstructor
 @Controller
@@ -13,4 +18,14 @@ public class UserController {
     private final HttpSession session;
     private final UserService userService;
 
+    @PostMapping("/join")
+    public String join(@Valid UserRequest.JoinDTO joinDTO, Errors errors) {
+        userService.회원가입(joinDTO);
+        return "redirect:/login-form";
+    }
+
+    @GetMapping("/join-form")
+    public String joinForm() {
+        return "user/join-form";
+    }
 }
